0814d474055bed69f87560ac86614d0d6af7522a
kcheungag <114054077+kcheungag@users.noreply.github.com>
2023-11-20 21:04:19 -0500

    Basic UI of SearchScreen None Logic is done

--- app/src/main/java/com/example/team6/MainActivity.kt
+++ app/src/main/java/com/example/team6/MainActivity.kt
@@ -9,8 +9,19 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        //setContentView(R.layout.activity_main)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.search.setOnClickListener {
+            performSearch()
+        }
+
     }
+
+    private fun performSearch() {
+        //
+    }
+
+
+
 }
