a72bf4b9b5fac7267be99ed6c8e1f197ef5c6e83
Dayeeta <dayeetaganguly@yahoo.com>
2023-11-21 17:02:19 -0500

    Initial commit

---
+++ app/src/main/java/com/example/team6/models/user.kt
@@ -0,0 +1,8 @@
+package com.example.team6.models
+
+class user(val name:String, val email: String, val password: String, val membership:String) {
+    override fun toString(): String {
+        return "user(name='$name', email='$email', password='$password', membership='$membership')"
+    }
+
+}
